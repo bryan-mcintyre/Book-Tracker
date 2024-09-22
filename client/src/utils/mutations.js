379d34a27f713918f2229a8d5b6e138d62c21997
bryan-mcintyre <167257103+bryan-mcintyre@users.noreply.github.com>
2024-09-22 16:00:54 -0500

    updated mutations.js

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -24,3 +24,20 @@ export const LOGIN_USER = gql`
     }
 `
 
+export const SAVE_BOOK = gql`
+    mutation saveBook($authors: [String]!, $description: String!, $title: String!, $bookId: String!, $image: String!, $link: String!) {
+        saveBook(authors: $authors, description: $description, title: $title, bookId: $bookId, image: $image, link: $link) {
+            user
+            savedBooks
+        }
+    }
+`
+
+export const REMOVE_BOOK = gql`
+    mutation removeBook($bookId: String!) {
+        removeBook(bookId: $bookId) {
+            user
+            savedBooks
+        }
+    }
+`
