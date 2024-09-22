339349e2c6437615cdc2d266b290334e1c5359ac
bryan-mcintyre <167257103+bryan-mcintyre@users.noreply.github.com>
2024-09-21 19:06:37 -0500

    Added schemas folder with index.js, resolvers.js, and typeDefs.js

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,65 @@
+const { User } = require('../models')
+const { signToken, AuthenticationError } = require('../utils/auth')
+
+const resolvers = {
+    Query: {
+        me: async (parent, args, context) => {
+            if(context.user) {
+                return User.findOne({ _id: context.user._id })
+            }
+            throw AuthenticationError
+        }
+    },
+
+    Mutation: {
+        addUser: async (parent, { username, email, password }) => {
+            const user = await User.create({ username, email, password })
+            const token = signToken(user)
+
+            return { token, user }
+        },
+        login: async (parent, { email, password }) => {
+            const user = await User.findOne({ email })
+
+            if(!user) {
+                throw AuthenticationError
+            }
+
+            const correctPw = await user.isCorrectPassword(password)
+
+            if(!correctPw) {
+                throw AuthenticationError
+            }
+
+            const token = signToken(user)
+            return { token, user }
+        },
+        saveBook: async (parent, { userId, book }, context) => {
+            if(context.user) {
+                return User.findOneAndUpdate(
+                    { _id: userId },
+                    {
+                        $addToSet: { savedBooks: book }
+                    },
+                    {
+                        new: true,
+                        runValidators: true
+                    }
+                )
+            }
+            throw AuthenticationError
+        },
+        deleteBook: async (parent, { book }, context) => {
+            if(context.user) {
+                return User.findOneAndUpdate(
+                    { _id: context.user.id },
+                    { $pull: { savedBooks: book } },
+                    { new: true }
+                )
+            }
+            throw AuthenticationError
+        }
+    }
+}
+
+module.exports = resolvers
