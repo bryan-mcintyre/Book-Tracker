58a7aeac96b82b66988b0e70921060b35b55f3cb
bryan-mcintyre <167257103+bryan-mcintyre@users.noreply.github.com>
2024-09-26 19:21:05 -0500

    Updated TypeDefs again

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,23 +1,24 @@
 const typeDefs = `#graphql
     type User {
         _id: ID
-        username: String
-        email: String
-        bookCount: [Int]!
+        username: String,
+        email: String,
+        password: String
+        bookCount: [Int]!,
         savedBooks: [Book]
     }
 
     type Book {
-        bookId: String
-        authors: [String]!
-        description: String
-        title: String
-        image: String
+        bookId: String,
+        authors: [String]!,
+        description: String,
+        title: String,
+        image: String,
         link: String
     }
 
     type Auth {
-        token: ID!
+        token: ID!,
         user: User
     }
 
@@ -26,9 +27,9 @@ const typeDefs = `#graphql
     }
 
     type Mutation {
-        loginUser(email: String!, password: String!): Auth
-        addUser(username: String!, email: String!, password: String!): Auth
-        saveBook(authors: [String]!, description: String!, title: String!, bookId: String!, image: String!, link: String!): User
+        loginUser(email: String!, password: String!): Auth,
+        addUser(username: String!, email: String!, password: String!): Auth,
+        saveBook(authors: [String]!, description: String!, title: String!, bookId: String!, image: String!, link: String!): User,
         removeBook(bookId: String!): User
     }
 `
